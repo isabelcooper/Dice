558676a131dfc1b423216d59ee6e5c162dc6dc94
isabelcooper <isabel@triptease.com>
2019-01-10 11:52:06 +0000

    refactored and failing test for pretty print

--- lib/dice.rb
+++ lib/dice.rb
@@ -1,13 +1,16 @@
 class Dice
-#  attr_reader :number_of_rolls
 
-   # def initialize(number_of_rolls = 1)
-   #   @number_of_rolls = number_of_rolls
-   # end
+  DICE_SIDES = 6
 
   def roll(number_of_rolls = 1)
     result = []
-    number_of_rolls.times { result << rand(1..6)}
+    number_of_rolls.times { result << rand(DICE_SIDES)}
     result
   end
+
+#not passing
+  def pretty_print(result)
+  #  "You rolled a #{result.to_s}"
+  end
+
 end
--- spec/dice_spec.rb
+++ spec/dice_spec.rb
@@ -1,7 +1,7 @@
 require 'dice'
-require_relative 'spec_helper'
 
 describe Dice do
+
   before(:all) do
     @dice = Dice.new
     @result = @dice.roll
@@ -16,10 +16,6 @@ describe Dice do
     expect(subject).to respond_to(:roll).with(1).argument
   end
 
-  # it 'should output a number on roll' do
-  #   expect(@result).to be_a Fixnum
-  # end
-
   it 'should output a random number (1..6) on roll' do
     batch1 = []
     batch2 = []
@@ -29,23 +25,26 @@ describe Dice do
   end
 
   it 'should return a random number (srand test)' do
-    expect(@result).to eq [3]
+    expect(@result).to eq [2]
   end
 
    it 'should output an array on 2 or more rolls' do
      expect(@result).to be_a_kind_of Array
    end
 
-  # it 'should output a fixed number between 1 and 6 on roll' do
-  #     expect(1..6).to cover(@result)
-  # end
-
   it 'should output an array to the size of the number of rolls' do
     expect(@result2.count).to eq(2)
   end
 
-  # it 'should output an array of random numbers between 1 and 6 on roll' do
-  #   expect([1..6]).to include( @result2 )
-  # end
+  it 'should output an array of random numbers between 1 and 6 on roll' do
+    expect(@result2).to eq [1,3]
+  end
+
+  it 'should pretty print the results of the roll(s)' do
+  #  expect(@dice).to respond_to(:pretty_print)
+     expect(@dice.pretty_print(@result)).to eq "You rolled a 3"
+  #  expect(@dice.pretty_print(@result)).to output("You rolled a 2").to_stdout
+  #  expect(@result2.pretty_print).to eq "You rolled a 2 and a 4"
+  end
 
 end
